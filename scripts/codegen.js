0161e836b77e0db444a41f0fd622bff223d7b5a3
Dan Lynch <pyramation@gmail.com>
2022-08-22 11:03:38 -0700

    codegen scripts

---
+++ scripts/codegen.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+
+const { join } = require('path');
+const telescope = require('@osmonauts/telescope').default;
+
+telescope({
+    protoDirs: [
+        'wasmd-0.25/proto',
+        'wasmd-0.25/third_party/proto',
+        'cosmos-sdk-0.45/proto',
+        'cosmos-sdk-0.45/third_party/proto'
+    ],
+    outPath: join(__dirname, '/../src'),
+    options: {
+        includePackageVar: true,
+        bundle: {
+            enabled: false
+        },
+        excluded: {
+            protos: [
+                'cosmos/authz/v1beta1/event.proto',
+                'ibc/lightclients/solomachine/v2/solomachine.proto',
+                'tendermint/libs/bits/types.proto',
+                'cosmos/crypto/secp256r1/keys.proto'
+            ]
+        },
+        typingsFormat: {
+            useExact: true,
+            timestamp: 'timestamp',
+            duration: 'duration'
+        },
+        lcdClients: {
+            enabled: false
+        },
+        rpcClients: {
+            enabled: true,
+            bundle: false,
+            camelCase: false
+        },
+        aminoEncoding: {
+            enabled: false
+        }
+    }
+}).then(() => {
+    console.log('✨ All Done!');
+}).catch(e=>{
+    console.error(e);
+    process.exit(1);
+})
