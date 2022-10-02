09f51fb24fc199ae25a7e1453077ac329063660b
Dan Lynch <pyramation@gmail.com>
2022-10-02 18:26:57 -0500

    remove ts-proto dependencies and add telescope

---
+++ scripts/codegen.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+const { join } = require('path');
+const telescope = require('@osmonauts/telescope').default;
+
+telescope({
+    protoDirs: [
+        'wasmd-0.25/proto',
+        'wasmd-0.25/third_party/proto',
+        'cosmos-sdk-0.45/proto',
+        'cosmos-sdk-0.45/third_party/proto'
+    ],
+    outPath: join(__dirname, '/../src'),
+    options: {
+        includePackageVar: true,
+        typingsFormat: {
+            useExact: true,
+            timestamp: 'timestamp',
+            duration: 'duration'
+        },
+        lcdClients: {
+            enabled: false
+        },
+        rpcClients: {
+            enabled: true,
+            camelCase: false
+        },
+        aminoEncoding: {
+            enabled: false
+        }
+    }
+}).then(() => {
+    console.log('✨ All Done!');
+}, (e) => {
+    console.error(e);
+    process.exit(1);
+});
