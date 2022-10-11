e90039261c18c87b8629f4e4e7d796ca3655a6f2
Dan Lynch <pyramation@gmail.com>
2022-10-11 16:44:51 -0700

    pagination

--- src/cosmos/base/query/v1beta1/pagination.ts
+++ src/cosmos/base/query/v1beta1/pagination.ts
@@ -18,20 +18,20 @@ export interface PageRequest {
    * querying the next page most efficiently. Only one of offset or key
    * should be set.
    */
-  key?: Uint8Array;
+  key: Uint8Array;
   /**
    * offset is a numeric offset that can be used when key is unavailable.
    * It is less efficient than using key. Only one of offset or key should
    * be set.
    */
 
-  offset?: Long;
+  offset: Long;
   /**
    * limit is the total number of results to be returned in the result page.
    * If left empty it will default to a value to be set by each app.
    */
 
-  limit?: Long;
+  limit: Long;
   /**
    * count_total is set to true  to indicate that the result set should include
    * a count of the total number of items available for pagination in UIs.
@@ -39,14 +39,14 @@ export interface PageRequest {
    * is set.
    */
 
-  countTotal?: boolean;
+  countTotal: boolean;
   /**
    * reverse is set to true if results are to be returned in the descending order.
    *
    * Since: cosmos-sdk 0.43
    */
 
-  reverse?: boolean;
+  reverse: boolean;
 }
 /**
  * PageResponse is to be embedded in gRPC response messages where the
@@ -63,7 +63,7 @@ export interface PageResponse {
    * next_key is the key to be passed to PageRequest.key to
    * query the next page most efficiently
    */
-  nextKey?: Uint8Array;
+  nextKey: Uint8Array;
   /**
    * total is total number of results available if PageRequest.count_total
    * was set, its value is undefined otherwise
