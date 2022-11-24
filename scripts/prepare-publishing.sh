c96d8a06fa930a7b6eee89cc30fe280fb9a4c4f3
Simon Warta <simon@warta.it>
2022-11-24 17:02:27 +0100

    Get helpers.* bundled

--- scripts/prepare-publishing.sh
+++ scripts/prepare-publishing.sh
@@ -3,8 +3,14 @@ set -o errexit -o nounset -o pipefail
 command -v shellcheck >/dev/null && shellcheck "$0"
 
 DIRS="confio cosmos cosmos_proto cosmwasm gogoproto google ibc tendermint"
+FILES="helpers.d.ts helpers.js helpers.js.map"
 
 for dir in $DIRS; do
   rm -rf "$dir"
   cp -R "./build/$dir" ./
+done
+
+for f in $FILES; do
+  rm -rf "$f"
+  cp "./build/$f" ./
 done
