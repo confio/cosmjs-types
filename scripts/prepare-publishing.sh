5f48b069cd598b7c51e441cae6e16be5bc15b268
Simon Warta <simon@warta.it>
2023-10-03 16:29:42 +0200

    Add binary/utf8/varint files to build system

--- scripts/prepare-publishing.sh
+++ scripts/prepare-publishing.sh
@@ -4,12 +4,21 @@ command -v shellcheck >/dev/null && shellcheck "$0"
 
 DIRS=(cosmos cosmos_proto cosmwasm gogoproto google ibc tendermint)
 FILES=(
+  binary.d.ts
+  binary.js
+  binary.js.map
   helpers.d.ts
   helpers.js
   helpers.js.map
   index.d.ts
   index.js
   index.js.map
+  utf8.d.ts
+  utf8.js
+  utf8.js.map
+  varint.d.ts
+  varint.js
+  varint.js.map
 )
 
 for dir in "${DIRS[@]}"; do
